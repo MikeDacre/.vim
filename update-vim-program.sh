6f2bbc6f0a4eb244df79bd757a4d93b576b945e9
Mike Dacre <mike.dacre@gmail.com>
2011-11-11 12:35:03 -0800

    Improved update vim script

---
+++ update-vim-program.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env zsh 
+#===============================================================================
+#
+#          FILE:  update-vim.zsh
+# 
+#         USAGE:  ./update-vim.zsh 
+# 
+#   DESCRIPTION:  Simple script to update vim
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: Mike Dacre
+#       LICENCE: Copyright (c) 2011, Mike Dacre
+#       COMPANY: 
+#       CREATED: 11/11/2011 12:00:40 PM PST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+hg clone https://vim.googlecode.com/hg/ vim-program;
+
+cd vim-program;
+
+if [[ "$VENDOR" == 'apple' ]]; then
+  ./configure --prefix=/usr/local --enable-perlinterp=yes --enable-rubyinterp --enable-pythoninterp=yes --enable-python3interp=yes --enable-cscope --enable-gui=no --with-compiledby=Mike --enable-multibyte;
+elif [[ "$VENDOR" == 'unknown' ]]; then
+  ./configure --prefix=/usr --enable-perlinterp=yes --enable-rubyinterp --enable-pythoninterp=yes --enable-python3interp=yes --enable-cscope --enable-gui=no --with-compiledby=Mike --enable-multibyte;
+fi
+make;
+
+sudo make install;
+
+cd ..;
+rm -rf vim-program;
+
+echo "Done!";
+
