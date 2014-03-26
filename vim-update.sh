6f69e7d1a388e083cded712f777c151f50584fcb
Mike Dacre <mike.dacre@gmail.com>
2014-03-25 18:18:04 -0700

    Got rid of error checking code, it doesn't work

--- vim-update.sh
+++ vim-update.sh
@@ -8,50 +8,18 @@
 VIMLOCATION=$HOME
 
 mkdir -p $VIMLOCATION
-if [ !"$?" ] ; then
-  echo "Install location $VIMLOCATION doesn't exist and cannot be created. Maybe change the $VIMLOCATION variable in this script?"
-  exit
-fi
 
 hg clone https://vim.googlecode.com/hg/ vim-program
-if [ !"$?" ] ; then
-  echo "Download failed"
-  exit
-fi
 
 cd vim-program
-if [ !"$?" ] ; then
-  echo "Couldn't enter download directory"
-  exit
-fi
 
 ./configure --with-features=huge --prefix=$VIMLOCATION --enable-perlinterp=yes --enable-rubyinterp --enable-pythoninterp --enable-cscope --disable-gui --without-x --with-compiledby=Mike --enable-multibyte
-if [ !"$?" ] ; then
-  echo "Configure failed"
-  cd ..
-  exit
-fi
 
 make
-if [ !"$?" ] ; then
-  echo "Make failed"
-  cd ..
-  exit
-fi
 
 make install
-if [ !"$?" ] ; then
-  echo "Make install failed"
-  cd ..
-  exit
-fi
 
 cd ..
 rm -rf vim-program
-if [ !"$?" ] ; then
-  echo "Couldn't remove download directory"
-  cd ..
-  exit
-fi
 
 echo "Done!"
