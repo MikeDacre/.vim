5c06ec359fb1bf7cb5ac05bf4a4a2bd69f3b0fb7
Mike Dacre <mike.dacre@gmail.com>
2014-07-21 10:39:28 -0700

    Fixed tempalte bug

--- update.sh
+++ update.sh
@@ -13,7 +13,7 @@
 #       LICENCE: Open Source
 #       CREATED: 11/10/2011 22:16:34 PST
 #      REVISION: 1.1
-# Last modified: 2014-07-21 10:10
+# Last modified: 2014-07-21 10:12
 #===============================================================================
 
 VIM_HOME="$HOME/.vim"
@@ -35,7 +35,7 @@ fix_plugin_bugs () {
 
   # Link new templates
   cd $VIM_HOME/templates
-  for i in \=template.*; do
+  for i in \=template\=*; do
     cd $VIM_HOME/bundle/vim-template/templates/
     rm $i 2>/dev/null 
     ln -f -s $VIM_HOME/templates/$i $i
