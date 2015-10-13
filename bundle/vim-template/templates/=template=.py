3d306faa6217cd762aab53b1207cbddf3f10fa37
Mike Dacre <mike.dacre@gmail.com>
2015-10-13 11:46:17 -0700

    Python template fix

--- bundle/vim-template/templates/=template=.py
+++ bundle/vim-template/templates/=template=.py
@@ -1,1 +1,23 @@
-../../../templates/=template=.py
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 tabstop=4 expandtab shiftwidth=4 softtabstop=4
+"""
+#============================================================================#
+#                                                                            #
+#          FILE: %FILE% (python 3)                                                #
+#        AUTHOR: Michael D Dacre, mike.dacre@gmail.com                       #
+#  ORGANIZATION: Stanford University                                         #
+#       LICENSE: MIT License, property of Stanford, use as you wish          #
+#       VERSION: 0.1                                                         #
+#       CREATED: %FDATE%                                            #
+# Last modified: %FDATE%                                            #
+#                                                                            #
+#   DESCRIPTION: %HERE%                                                            #
+#                                                                            #
+#         USAGE: Run as a script or import as a module.  See '-h' or 'help'  #
+#                                                                            #
+#============================================================================#
+"""
+
+##
+# The End #
